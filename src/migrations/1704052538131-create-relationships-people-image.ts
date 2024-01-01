565dc72d032999a200ff1be47eea24d896025b14
Vitalii Fetisov <neverest1827@gmail.com>
2024-01-01 19:15:33 +0200

    created a migration for the people and image entities and set up an application between them

---
+++ src/migrations/1704052538131-create-relationships-people-image.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateRelationshipsPeopleImage1704052538131 implements MigrationInterface {
+    name = 'CreateRelationshipsPeopleImage1704052538131'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`image_entity\` ADD \`peopleId\` int NULL`);
+        await queryRunner.query(`ALTER TABLE \`image_entity\` ADD CONSTRAINT \`FK_9f99b37067756265e1f432a5b02\` FOREIGN KEY (\`peopleId\`) REFERENCES \`people_entity\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`image_entity\` DROP FOREIGN KEY \`FK_9f99b37067756265e1f432a5b02\``);
+        await queryRunner.query(`ALTER TABLE \`image_entity\` DROP COLUMN \`peopleId\``);
+    }
+
+}
